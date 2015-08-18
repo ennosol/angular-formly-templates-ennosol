eac862a3c1d6f1c560e3acf558069387ca7e5a97
Konrad Kiss <konrad@konradkiss.com>
2015-08-18 23:50:27 +0200

    Added a better example, many new types and a few special types.

--- src/modules/angular-formly-templates-ennosol.js
+++ src/modules/angular-formly-templates-ennosol.js
@@ -1,70 +1,59 @@
-angular.module('formlyEnnosol', ['formly'], function configFormlyEnnosol(formlyConfigProvider) {
+angular.module('formlyEnnosol', ['formly', 'NgSwitchery'], function configFormlyEnnosol(formlyConfigProvider) {
     'use strict';
 
     // WRAPPERS
-    formlyConfigProvider.setWrapper([
-        {
-            name: 'label',
-            templateUrl: 'templates/label.html'
-        }
-    ]);
+    formlyConfigProvider.setWrapper([{
+        name: 'label',
+        templateUrl: '/src/templates/label.html'
+    }, {
+        name: 'addons',
+        templateUrl: '/src/templates/addons.html'
+    }]);
 
     // TYPES
-    formlyConfigProvider.setType([
-        {
-            name: 'checkbox',
-            templateUrl: 'templates/checkbox.html',
-            wrapper: ['label']
-        },
-        {
-            name: 'input',
-            templateUrl: 'templates/input.html',
-            wrapper: ['label']
-        },
-        {
-            name: 'radio',
-            templateUrl: 'templates/radio.html'
-        },
-        {
-            name: 'range',
-            templateUrl: 'templates/range.html'
-        },
-        {
-            name: 'textarea',
-            templateUrl: 'templates/textarea.html'
-        },
-        {
-            name: 'toggle',
-            templateUrl: 'templates/toggle.html'
-        },
-        {
-            name: 'select',
-            templateUrl: 'templates/select.html'
-        }
-    ]);
-
-    // MISC
-    formlyConfigProvider.templateManipulators.preWrapper.push(function ariaDescribedBy(template, options, scope) {
-        if (options.templateOptions && angular.isDefined(options.templateOptions.description) &&
-            options.type !== 'radio' && options.type !== 'checkbox') {
-            var el = angular.element('<a></a>');
-            el.append(template);
-            var modelEls = angular.element(el[0].querySelectorAll('[ng-model]'));
-            if (modelEls) {
-                el.append(
-                    '<p id="' + scope.id + '_description"' +
-                    'class="help-block"' +
-                    'ng-if="options.templateOptions.description">' +
-                    '{{options.templateOptions.description}}' +
-                    '</p>'
-                );
-                modelEls.attr('aria-describedby', scope.id + '_description');
-                return el.html();
-            } else {
-                return template;
-            }
-        } else {
-            return template;
-        }
-    });
+    formlyConfigProvider.setType([{
+        name: 'checkbox',
+        templateUrl: '/src/templates/checkbox.html',
+        wrapper: []
+    }, {
+        name: 'switch',
+        templateUrl: '/src/templates/switch.html',
+        wrapper: ['label']
+    }, {
+        name: 'radio',
+        templateUrl: '/src/templates/radio.html',
+        wrapper: ['label']
+    }, {
+        name: 'radio-inline',
+        templateUrl: '/src/templates/radio-inline.html',
+        wrapper: ['label']
+    }, {
+        name: 'input',
+        templateUrl: '/src/templates/input.html',
+        wrapper: ['addons', 'label']
+    }, {
+        name: 'date',
+        templateUrl: '/src/templates/date.html',
+        wrapper: ['addons', 'label']
+    }, {
+        name: 'daterange',
+        templateUrl: '/src/templates/daterange.html',
+        wrapper: ['addons', 'label']
+    }, {
+        name: 'coordinate',
+        templateUrl: '/src/templates/coordinate.html',
+        wrapper: ['addons', 'label']
+    }, {
+        name: 'static',
+        templateUrl: '/src/templates/static.html',
+        wrapper: ['addons', 'label']
+    }, {
+        name: 'textarea',
+        templateUrl: '/src/templates/textarea.html',
+        wrapper: ['addons', 'label']
+    }, {
+        name: 'select',
+        templateUrl: '/src/templates/select.html',
+        wrapper: ['addons', 'label']
+    }]);
 });
