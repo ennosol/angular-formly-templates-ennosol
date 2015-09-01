568546ad95799de68713dfa07a3c14daaec9f682
Konrad Kiss <konrad@konradkiss.com>
2015-09-01 12:03:45 +0200

    Removing the dotModel initiative, adding a template that supports true object nesting in the model. Full vm capability. :)

--- src/modules/angular-formly-templates-ennosol.js
+++ src/modules/angular-formly-templates-ennosol.js
@@ -8,10 +8,20 @@ angular.module('formlyEnnosol', ['formly', 'NgSwitchery', 'tsSelect2'], ['formly
     }, {
         name: 'addons',
         templateUrl: '/src/templates/addons.html'
+    }, {
+        name: 'fieldset',
+        templateUrl: '/src/templates/fieldset.html'
+    }, {
+        name: 'validation',
+        templateUrl: '/src/templates/error.html'
     }]);
 
     // TYPES
     formlyConfigProvider.setType([{
+        name: 'nested',
+        wrapper: ['fieldset'],
+        template: '<formly-form model="model[options.key]" fields="options.data.fields"></formly-form>'
+    }, {
         name: 'checkbox',
         templateUrl: '/src/templates/checkbox.html',
         wrapper: []
@@ -34,15 +44,15 @@ angular.module('formlyEnnosol', ['formly', 'NgSwitchery', 'tsSelect2'], ['formly
     }, {
         name: 'date',
         templateUrl: '/src/templates/date.html',
-        wrapper: ['addons', 'label']
+        wrapper: ['addons', 'label', 'validation']
     }, {
         name: 'daterange',
         templateUrl: '/src/templates/daterange.html',
-        wrapper: ['addons', 'label']
+        wrapper: ['addons', 'label', 'validation']
     }, {
         name: 'coordinate',
         templateUrl: '/src/templates/coordinate.html',
-        wrapper: ['addons', 'label']
+        wrapper: ['addons', 'label', 'validation']
     }, {
         name: 'static',
         templateUrl: '/src/templates/static.html',
@@ -50,11 +60,11 @@ angular.module('formlyEnnosol', ['formly', 'NgSwitchery', 'tsSelect2'], ['formly
     }, {
         name: 'textarea',
         templateUrl: '/src/templates/textarea.html',
-        wrapper: ['addons', 'label']
+        wrapper: ['addons', 'label', 'validation']
     }, {
         name: 'spinner',
         templateUrl: '/src/templates/spinner.html',
-        wrapper: ['addons', 'label']
+        wrapper: ['addons', 'label', 'validation']
     }, {
         name: 'search',
         templateUrl: '/src/templates/search.html',
@@ -113,96 +123,6 @@ angular.module('formlyEnnosol', ['formly', 'NgSwitchery', 'tsSelect2'], ['formly
 })
 
 .service('formlyEnnosolCfg', ['$q', '$http', function($q, $http) {
-
-    // This allows the controller to handle dot notation syntax
-    // when addressing the model.
-    // This also disallows the use of dots in keys
-    this.initDotModel = function(scope, model, dotModel) {
-
-        var self = this;
-
-        // Access an object's properties via dot notation
-        // Creates any keys not yet present as objects
-        self.dotGet = function(o, s) {
-            s = s.replace(/\[(\w+)\]/g, '--$1');
-            s = s.replace(/^--/, '');
-            var a = s.split('--');
-            for (var i = 0, n = a.length; i < n; ++i) {
-                var k = a[i];
-                if (!(k in o)) {
-                    o[k] = {};
-                }
-                o = o[k];
-            }
-            return o;
-        };
-
-        self.dotSet = function(object, dotNotation, value) {
-
-            var segments = dotNotation.split('--');
-            var segLen = segments.length;
-
-            for (var i = 0; i < segLen; ++i) {
-                var seg = segments[i];
-
-                if (Object.prototype.toString.call(object) !== '[object Object]') {
-                    continue;
-                }
-
-                if (!(seg in object)) {
-                    object[seg] = {};
-                }
-
-                if (i === segLen - 1) {
-                    object[seg] = value;
-                } else {
-                    object = object[seg];
-                }
-            }
-        };
-
-        if (typeof model === 'undefined') {
-            model = 'model';
-        }
-
-        if (Object.prototype.toString.call(scope[model]) !== '[object Object]') {
-            scope[model] = {};
-        }
-
-        scope.$watch(model, function(value) {
-
-            scope[dotModel] = {};
-
-            if (Object.prototype.toString.call(value) !== '[object Object]') {
-                return;
-            }
-
-            // Update the model
-            for (var prop in value) {
-
-                var oprop = prop;
-                prop = prop.replace(/\[(\w+)\]/g, '--$1');
-                prop = prop.replace(/^--/, '');
-
-                if (prop.indexOf('--') > 0) {
-                    self.dotSet(scope[model], prop, value[prop]);
-                }
-            }
-
-            // Update the dot copy of the model (this is read-only!)
-            for (var prop in value) {
-
-                var oprop = prop;
-                prop = prop.replace(/\[(\w+)\]/g, '--$1');
-                prop = prop.replace(/^--/, '');
-
-                if (prop.indexOf('--') <= 0) {
-                    scope[dotModel][prop] = scope[model][prop];
-                }
-            }
-
-        }, true);
-    };
 
     this.configuration = function() {
 
